ff449d8726ab8dc97da62e92d09289251e02d869
Claire Taylor-Cuthbertson <Clairetc36@gmail.com>
2021-08-11 20:27:16 +1000

    Bounce material and simple setup for Ball(bouncing off paddles and wall + colliding with score zone)

--- Assets/Scripts/Controls/Ball.cs
+++ Assets/Scripts/Controls/Ball.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    new Rigidbody2D rigidbody;
+
+    //[Range(1f, 10f)]
+     float speed = 15f;
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = Vector2.right * speed;
+        //wait two second before shooting the ball
+        //Invoke("BeginRound", 2.0f);
+    }
+    void BeginRound(){
+        float randomValue = Random.value;
+        if(randomValue >= 0.5f)
+            rigidbody.AddForce(new Vector2(20, -15));
+        else
+            rigidbody.AddForce(new Vector2(-20, -15));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter2D (Collision2D coll) {
+        if(coll.collider.CompareTag("Player")){
+            Vector2 vel;
+            vel.x = rigidbody.velocity.x;
+            vel.y = (rigidbody.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+            rigidbody.velocity = vel;
+        }      
     }
 }
