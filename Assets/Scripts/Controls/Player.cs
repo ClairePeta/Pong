9fa6013069e8088eaa1d2ea7933e7f75daa37125
Claire Taylor-Cuthbertson <Clairetc36@gmail.com>
2021-08-11 19:37:11 +1000

    Layout scripts, Start player paddle movement

---
+++ Assets/Scripts/Controls/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool player1 = true;
+    new Rigidbody2D rigidbody;
+    [Range(1f, 10f)]
+    public float speed = 5f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(player1){
+            var vel = rigidbody.velocity;
+            if (Input.GetKey(KeyCode.UpArrow)) { vel.y = speed; }  /*move up*/ 
+            else if (Input.GetKey(KeyCode.DownArrow)) { vel.y = -speed; } /*move down*/
+            else { vel.y = 0; } /*stop moving*/
+            rigidbody.velocity = vel;
+        }else{
+            var vel = rigidbody.velocity;
+            if (Input.GetKey(KeyCode.W)) { vel.y = speed; }  /*move up*/ 
+            else if (Input.GetKey(KeyCode.S)) { vel.y = -speed; } /*move down*/
+            else { vel.y = 0; } /*stop moving*/
+            rigidbody.velocity = vel;
+        }
+            Vector2 pos = (Vector2)transform.position;
+            if (pos.y > 4.2f) { pos.y = 4.2f; } /*upper bounds*/
+            else if (pos.y < -4.2f) { pos.y = -4.2f; } /*lower bounds*/
+            transform.position = pos; 
+    }
+}
---
+++ Assets/Scripts/ScoreZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == GameLayers.Ball){
+            //award the other player/AI a point
+
+        }     
+    }  
+}
