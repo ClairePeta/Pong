7f4feaee590bb30dc15e230c6bf912db6368b8cd
Claire Taylor-Cuthbertson <Clairetc36@gmail.com>
2021-08-12 19:08:06 +1000

    Clean unused

--- Assets/Scripts/GameLayers.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class GameLayers
-{
-    public const int Default = 0;
-    public const int TransparentFX = 1;
-    public const int IgnoreRaycast = 2;
-    public const int Water = 4;
-    public const int UI = 5;
-    public const int Ball = 8;
-}
